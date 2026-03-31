663eb7e60569dbb58899164c09fbe76f597cafcf
NayanMagar1 <nayanmagar04@gmail.com>
2026-03-31 19:39:14 +0545

    futsal list updated

--- guthi/views.py
+++ guthi/views.py
@@ -47,7 +47,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect("dashboard")  # you can change later
+            return redirect("home")  
         else:
             messages.error(request, "Invalid username or password")
             return redirect("login")
