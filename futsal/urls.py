be822ccc1b53460def7ef83512a4afc740759dfb
NayanMagar1 <nayanmagar04@gmail.com>
2026-04-22 08:47:20 +0545

    aobut

--- futsal/urls.py
+++ futsal/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-rlpatterns = [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('guthi.urls')),
 ]
